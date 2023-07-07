e35f42f05b2815bf77af1b6b22031f29dbbf471e
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2023-07-07 23:11:41 +0300

    Leaves Section
    Leaves Section

---
+++ src/components/Employee.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Employee(props) {
+  return (
+        <div>
+            {/* <p>{props.Employee.id}</p>
+            <p>{props.Employee.name}</p>
+            <img src={props.Employee.avatar}><p>{props.Employee.Department_role}</p>
+            <p>{props.Employee.Date}</p>
+            <p>{props.Employee.Status}</p>
+            <p>{props.Employee.Leave_From}</p>
+            <p>{props.Employee.Leave_To}</p>
+            <p>{props.Employee.Leave_type}</p>
+            <p>{props.Employee.Reason}</p>
+            <p>{props.Employee.Action}</p> */}
+        </div>
+    )
+}
+
+export default Employee;
--- src/pages/Leaves/Leaves.jsx
+++ src/pages/Leaves/Leaves.jsx
@@ -2,8 +2,8 @@ import './leaves.css'
 import {TopSearch} from "../../components/TopSerarch/TopSearch.jsx";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { useEffect, useState } from 'react';
-
+// import { useEffect, useState } from 'react';
+// import Employee from '../../components/Employee';
 
 export const Leaves = () => {
     // const [employees,,setemployee] = useState([]);
@@ -13,25 +13,75 @@ export const Leaves = () => {
     //     .then((response) => response.json)
     //     .then((data) => console.log(employees))
     // },[])
+
+    // const EmployeesData = employees.map((employee) => {
+    //     return(
+    //         <Employee employee={employee}/>
+    //     );
+    // })
     return (
         <div className='leaves'>
             <TopSearch/>
-            <div className='leaves_container'>
-                <Row>
-                    <Col className='leaves_header'>Leaves Request</Col>
-                    <Col></Col>
+            <div className='leaves-container'>
+                <Row className=''>
+                    <Col className='leaves-header'>Leaves Request</Col>
+                    <Col>
+                        <div className='search-leaves'>
+                            <input className='leaves-search-input' type='search' placeholder='Search for Employees'/>
+                        </div>
+                    </Col>
                 </Row>
-                <Row>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
-                    <Col></Col>
+                <Row className='Leave-details d-flex'>
+                    <Col>
+                    <div className='id-leaves'>
+                        <p>id</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Name-leaves'>
+                        <p>Name</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Department_Role-leaves'>
+                        <p>Department Role</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Date-leaves'>
+                        <p>Date</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Status-leaves'>
+                        <p>Status</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Leave_From-leaves'>
+                        <p>Leave From</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Leave_To-leaves'>
+                        <p>Leave To</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Leave_Type-leaves'>
+                        <p>Leave Type</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Reason-leaves'>
+                        <p>Reason</p>
+                    </div>
+                    </Col>
+                    <Col>
+                    <div className='Action-leaves'>
+                        <p>Action</p>
+                    </div>
+                    </Col>
                 </Row>
             </div>
         </div>
