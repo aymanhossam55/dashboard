f1c7fd584f7ce2267f8f0ba02302333fd6d61a00
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2023-07-07 20:48:19 +0300

    leaves section
    leaves section

--- src/pages/Leaves/Leaves.jsx
+++ src/pages/Leaves/Leaves.jsx
@@ -1,11 +1,39 @@
 import './leaves.css'
 import {TopSearch} from "../../components/TopSerarch/TopSearch.jsx";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect, useState } from 'react';
+
 
 export const Leaves = () => {
+    // const [employees,,setemployee] = useState([]);
+
+    // useEffect(() => {
+    //     fetch()
+    //     .then((response) => response.json)
+    //     .then((data) => console.log(employees))
+    // },[])
     return (
         <div className='leaves'>
             <TopSearch/>
-            Leaves
+            <div className='leaves_container'>
+                <Row>
+                    <Col className='leaves_header'>Leaves Request</Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+            </div>
         </div>
     )
 }
